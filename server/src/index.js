610773eb4670df4c5c0aff15e75d037c8195d106
Maurice <mauriceboe@icloud.com>
2026-03-19 14:44:35 +0100

    Fix PDF export: allow same-origin iframes (X-Frame-Options)

--- server/src/index.js
+++ server/src/index.js
@@ -47,7 +47,7 @@ app.use(express.json());
 // Security headers
 app.use((req, res, next) => {
   res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
   res.setHeader('X-XSS-Protection', '1; mode=block');
   res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
   next();
